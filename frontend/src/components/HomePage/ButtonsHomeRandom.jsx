1c6b50e433589884d83ae36123779e399c93c981
PierreYves Gille <pierre-yves51@orange.fr>
2022-11-24 16:55:38 +0100

    add random button

---
+++ frontend/src/components/HomePage/ButtonsHomeRandom.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ButtonsHome() {
+  return (
+    <div className="positionbutton">
+      <button className="randomButton" type="button">
+        Random
+      </button>
+      <button className="randomButton" type="button">
+        Random2
+      </button>
+    </div>
+  );
+}
+export default ButtonsHome;
