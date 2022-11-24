77baa9585b4dc0271bfaaa0ade851b6c6becfbb4
PierreYves Gille <pierre-yves51@orange.fr>
2022-11-24 16:56:55 +0100

    add components to homepage

---
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ButtonsHome from "./ButtonsHomeRandom";
+import LogoHome from "./LogoHome";
+
+function HomePage() {
+  return (
+    <div className="logobuttons">
+      <LogoHome />
+      <ButtonsHome />
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ frontend/src/components/HomePage/LogoHome.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../../assets/logohome.png";
+
+function LogoHome() {
+  return (
+    <div>
+      <img src={logo} alt="No Traveler Logo" className="logohomes" />
+    </div>
+  );
+}
+
+export default LogoHome;
